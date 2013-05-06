872b5c03aef22243e6c78dcbec6a0f51e43238c8
Willi Ballenthin <willi.ballenthin@gmail.com>
2013-05-05 22:39:12 -0400

    update extract records as it appears to now work

--- extract_valid_evtx_records_and_templates.py
+++ extract_valid_evtx_records_and_templates.py
@@ -26,8 +26,6 @@ from lxml import etree
 from Evtx.Evtx import ChunkHeader
 from Evtx.Nodes import BXmlTypeNode
 
-from find_evtx_chunks import CHUNK_HIT_TYPE
-
 
 def to_lxml(record):
     """
@@ -47,7 +45,7 @@ def get_child(node, tag, ns="{http://schemas.microsoft.com/win/2004/08/events/ev
 
 
 def get_eid(record):
-    return get_eid(get_eid(to_lxml(record), "System"), "EventID")
+    return get_child(get_child(to_lxml(record), "System"), "EventID").text
 
 
 def get_template(root):
@@ -122,14 +120,21 @@ def main():
         description="Find offsets of EVTX chunk records.")
     parser.add_argument("--records_outfile", type=str, default="records.xml",
                         help="The path to the output file for records")
-    parser.add_argument("--templates_outfile", type=str, default="templates.xml",
+    parser.add_argument("--templates_outfile", type=str, default="templates.txt",
                         help="The path to the output file for templates")
+    parser.add_argument("--verbose", action="store_true",
+                        help="Enable debugging output")
     parser.add_argument("evtx", type=str,
                         help="Path to the Windows EVTX file")
     parser.add_argument("chunk_hits_file", type=str,
                         help="Path to the file containing output of"
                         " find_evtx_chunks.py")
     args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(name)s %(message)s")
+    
+    logger = logging.getLogger("extract_records")
 
     with open(args.evtx, "rb") as f:
         with contextlib.closing(mmap.mmap(f.fileno(), 0,
@@ -138,17 +143,19 @@ def main():
             templates = {}
             with open(args.chunk_hits_file, "rb") as g:
                 for line in g.read().split("\n"):
-                    hit_type, _, offset = line.partition("\n")
-                    if hit_type != CHUNK_HIT_TYPE.CHUNK_VALID:
+                    hit_type, _, offset = line.partition("\t")
+                    logger.debug("Processing line: %s, %s", hit_type, offset)
+                    if hit_type != "CHUNK_VALID":  # TODO(wb): I dont like string matching
+                        logging.debug("Skipping, cause its not a valid chunk")
                         continue
-                    offset = int(offset, 0x16)
+                    offset = int(offset, 0x10)
                     new_xml, new_templates = extract_chunk(buf, offset)
                     xml.append(new_xml)
 
                     # merge new templates into existing templates
                     for eid in new_templates.keys():
-                        offset, template = new_templates[eid]
-                        merge_template(templates, eid, offset, template)
+                        for offset, template in new_templates[eid]:
+                            merge_template(templates, eid, offset, template)
     with open(args.records_outfile, "wb") as f:
         f.write("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\" ?>")
         f.write("<Events>")
@@ -158,9 +165,9 @@ def main():
         for eid in sorted(templates.keys()):
             for offset, template in templates[eid]:
                 f.write("TEMPLATE\n")
-                f.write("EID: %s\n" % hex(eid))
+                f.write("EID: %s\n" % eid)
                 f.write("OFFSET: %s\n" % (offset))
-                f.write("\n%s\n\n" % template)
+                f.write("%s\n\n\n" % template)
 
 
 if __name__ == "__main__":
