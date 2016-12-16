990426c4ebb62ada3455d2ab9b5e5256e63cc076
Willi Ballenthin <willi.ballenthin@gmail.com>
2016-12-16 15:48:42 -0500

    create standard python module structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name="evtxtract",
+      version="0.2",
+      description="EVTXtract recovers and reconstructs fragments of EVTX log files from raw binary data, including unallocated space and memory images.",
+      author="Willi Ballenthin",
+      author_email="william.ballenthin@fireeye.com",
+      url="https://github.com/williballenthin/evtxtract",
+      license="Apache 2.0 License",
+      packages=setuptools.find_packages(),
+      install_requires=[
+          'pytest',
+          'python-evtx',
+      ],
+)
